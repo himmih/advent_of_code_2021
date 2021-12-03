a8ab87186c4af5e9ef7f7f74cbc0ec328449b990
mih <mshevelevich@gmail.com>
2021-12-04 00:19:07 +0300

    done part one

---
+++ src/main/java/ru/mih/day3/DiagnosticReport.java
@@ -0,0 +1,103 @@
+package ru.mih.day3;
+
+import akka.stream.javadsl.*;
+import akka.util.ByteString;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class DiagnosticReport {
+
+        int[] onesCount;
+        int lineCount;
+
+        public static DiagnosticReport START = new DiagnosticReport(new int[0], 0);
+
+        public DiagnosticReport(int[] ones, int lineCount) {
+            this.onesCount = ones;
+            this.lineCount = lineCount;
+        }
+
+        public CompletionStage<DiagnosticReport> scan(String line){
+
+            String onlyNumbersLine = line.trim(); //.replaceAll("[^01]", ""); odd symbols only whitespaces
+            int lineLength = onlyNumbersLine.length();
+            System.out.println("onesCount size: " + onesCount.length + " with line: " + onlyNumbersLine);
+
+            int[] newOnesCount;
+
+            if (lineCount == 0){
+                newOnesCount = new int[lineLength];
+                System.out.println("creating array with  size: " + lineLength);
+            }else {
+                if (onesCount.length != lineLength){
+                    throw new IllegalStateException("Wrong number in line: " + lineCount+1
+                            + " we have onesCount dimension : " + onesCount.length + " lineLength: " + lineLength);
+                }
+                newOnesCount = Arrays.copyOf(onesCount, onesCount.length);
+            }
+
+            for(int j = 0; j<onlyNumbersLine.length();j++){
+                if ('1' == onlyNumbersLine.charAt(j)) newOnesCount[j] += 1 ;
+            }
+
+            return CompletableFuture.supplyAsync(() -> new DiagnosticReport(newOnesCount, lineCount + 1));
+        }
+
+        @Override
+        public String toString() {
+            return String.format("GR(B): %s, GR(D): %d, ER(B): %s, ER(D): %d, PS(D): %d",
+                    getGammaRateBinary(), getGammaRate(),
+                    getEpsilonRateBinary(), getEpsilonRate(),
+                    getGammaRate()*getEpsilonRate());
+
+        }
+
+        public int getPowerConsumption(){
+            return getGammaRate()*getEpsilonRate();
+        }
+
+        public int getGammaRate() {
+            return Integer.parseInt(getGammaRateBinary(), 2);
+        }
+
+        public int getEpsilonRate() {
+            return Integer.parseInt(getEpsilonRateBinary(), 2);
+        }
+
+        public String getGammaRateBinary() {
+            StringBuffer gammaRateBinary = new StringBuffer();
+            for(int j = 0; j<this.onesCount.length; j++){
+                if (this.onesCount[j] > this.lineCount - this.onesCount[j]){ //1s > 0s
+                    gammaRateBinary.append("1");
+                }else if (this.onesCount[j] == this.lineCount - this.onesCount[j]){
+                    throw new IllegalStateException("1s == 0s (" + this.onesCount[j] + ") in index: " + j) ;
+                }else {
+                    gammaRateBinary.append("0");
+                }
+            }
+            return gammaRateBinary.toString();
+        }
+
+        public String getEpsilonRateBinary() {
+
+            return getGammaRateBinary()
+                    .replaceAll("1", "X")
+                    .replaceAll("0", "1")
+                    .replaceAll("X", "0");
+
+        }
+
+
+
+    public static Sink<ByteString, CompletionStage<DiagnosticReport>> foundConditions(){
+        return Flow.of(ByteString.class)
+                .filterNot(ByteString::isEmpty)
+                .map(bs -> bs.utf8String())
+                .foldAsync(DiagnosticReport.START, (acc, element) -> acc.scan(element))
+                .toMat(Sink.head(), Keep.right());
+    }
+
+
+}
---
+++ src/test/java/ru/mih/TestDay3.java
@@ -0,0 +1,81 @@
+package ru.mih;
+
+import akka.actor.ActorSystem;
+import akka.stream.javadsl.FileIO;
+import akka.stream.javadsl.Framing;
+import akka.stream.javadsl.FramingTruncation;
+import akka.util.ByteString;
+import org.junit.AfterClass;
+import org.junit.Test;
+import ru.mih.day3.DiagnosticReport;
+
+import java.nio.file.Paths;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+import static ru.mih.day3.DiagnosticReport.foundConditions;
+
+public class TestDay3 {
+
+    public static final ActorSystem testSystem = ActorSystem.create("TestSystem");
+
+    @AfterClass
+    public static void terminate(){
+        testSystem.terminate();
+    }
+
+
+    @Test
+    public void testPartOneExample() throws ExecutionException, InterruptedException, TimeoutException {
+        final CompletionStage<DiagnosticReport> future
+            = FileIO.fromPath(
+                    Paths.get("/home/mih/projects/advent_of_code_2021/src/main/resources/day3/test1.txt"))
+            .via(Framing.delimiter(ByteString.fromString("\n"), 256, FramingTruncation.ALLOW))
+                        .runWith(foundConditions(), testSystem);
+
+
+        final DiagnosticReport result = future.toCompletableFuture().get(3, TimeUnit.SECONDS);
+        assertEquals("10110", result.getGammaRateBinary());
+        assertEquals("01001", result.getEpsilonRateBinary());
+        assertEquals(22, result.getGammaRate());
+        assertEquals(9, result.getEpsilonRate());
+    }
+
+    @Test
+    public void testPartOneExample2() throws ExecutionException, InterruptedException, TimeoutException {
+        final CompletionStage<DiagnosticReport> future
+                = FileIO.fromPath(
+                        Paths.get("/home/mih/projects/advent_of_code_2021/src/main/resources/day3/test1.txt"))
+                .via(Framing.delimiter(ByteString.fromString("\n"), 256, FramingTruncation.ALLOW))
+                .runWith(foundConditions(), testSystem);
+
+
+        final DiagnosticReport result = future.toCompletableFuture().get(3, TimeUnit.SECONDS);
+        assertEquals("GR(B): 10110, GR(D): 22, ER(B): 01001, ER(D): 9, PS(D): 198", result.toString());
+        testSystem.terminate();
+
+    }
+
+    @Test
+    public void testPartOne() throws ExecutionException, InterruptedException, TimeoutException {
+        ActorSystem testSystem = ActorSystem.create("TestSystem2");
+        final CompletionStage<DiagnosticReport> future
+                = FileIO.fromPath(
+                        Paths.get("/home/mih/projects/advent_of_code_2021/src/main/resources/day3/test2.txt"))
+                .via(Framing.delimiter(ByteString.fromString("\n"), 256, FramingTruncation.ALLOW))
+                .runWith(foundConditions(), testSystem);
+
+
+        final DiagnosticReport result = future.toCompletableFuture().get(3, TimeUnit.SECONDS);
+
+        assertEquals("GR(B): 010010010110, GR(D): 1174, ER(B): 101101101001, ER(D): 2921, PS(D): 3429254", result.toString());
+
+        assertEquals(result.getGammaRate()* result.getEpsilonRate(), result.getPowerConsumption());
+
+        testSystem.terminate();
+
+    }
+}
